a577a193ca17e5dad4cea8a95394c7171680943a
Jinu <pcjinu@ymail.com>
2016-12-23 18:37:28 +0530

    checking

--- src/main/java/com/buya2z/controller/WelcomeController.java
+++ src/main/java/com/buya2z/controller/WelcomeController.java
@@ -28,7 +28,7 @@ public class WelcomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        req.getRequestDispatcher("index.html").include(req, resp);
+        req.getRequestDispatcher("welcome.jsp").forward(req, resp);
         //req.getRequestDispatcher("/index.html").include(req, resp);
     }
 
